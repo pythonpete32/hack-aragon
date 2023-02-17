728156dd3405354986171c56dfcd743a0ab3ff78
pythonpete32 <aaron@blackscale.io>
2023-02-17 14:51:46 +0400

    Add Aragon Context

--- ui/components/Providers.tsx
+++ ui/components/Providers.tsx
@@ -6,14 +6,17 @@ import { WagmiConfig } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { chains, wagmiClient } from "../lib/wagmiClient";
+import { AragonSDKWrapper } from "../lib/aragonContext";
 
 function Providers({ children }: { children: React.ReactNode }) {
   return (
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains} theme={darkTheme()}>
         <QueryClientProvider client={new QueryClient()}>
-          {children}
-          <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+          <AragonSDKWrapper>
+            {children}
+            <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+          </AragonSDKWrapper>
         </QueryClientProvider>
       </RainbowKitProvider>
     </WagmiConfig>
