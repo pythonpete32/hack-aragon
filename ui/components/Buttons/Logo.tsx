1ed85e89e97b0ee9119374cafadf890e90721161
pythonpete32 <aaron@blackscale.io>
2023-02-17 13:09:38 +0400

    modify the logo

--- ui/components/Buttons/Logo.tsx
+++ ui/components/Buttons/Logo.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Logo() {
   return (
     <Link href="/">
-      <div className="btn flex justify-start btn-ghost no-animation normal-case text-2xl w-full">
-        🧪 Hack Web3
+      <div className="btn flex justify-start btn-ghost no-animation normal-case text-3xl font-semibold w-full">
+        🦅 Hack OSx
       </div>
     </Link>
   );
