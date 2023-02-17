9049e993a73c7f921890e0b8013e3c11a0785726
pythonpete32 <aaron@blackscale.io>
2023-02-17 13:10:23 +0400

    add font

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sono)"],
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
